2e16c802df350cfd49e005ed0c8bf7a39797e4d3
Ailton Loures Ferreira <ailton.ferreira@stoneage.com.br>
2023-07-09 19:32:04 -0300

    adicionando increment e decrement ao countdown

--- src/frontend/renderer.js
+++ src/frontend/renderer.js
@@ -6,6 +6,8 @@ const countdownMinuteSpan = document.getElementById('countdown-minute')
 const countdownSecondSpan = document.getElementById('countdown-second')
 const countdownStartButton = document.getElementById('countdown-start')
 const countdownResetButton = document.getElementById('countdown-reset')
+const countdownIncrementButton = document.getElementById('countdown-increment')
+const countdownDecrementButton = document.getElementById('countdown-decrement')
 
 // * globals object
 const _api = window.api
@@ -45,6 +47,24 @@ const startCountdown = () => {
   }
 }
 
+const incrementCountdown = () => {
+  const countdownIncrement = _data.countdownTimeInMinutes + 5
+  if (countdownIncrement > 60) return
+
+  _data.countdownTimeInMinutes = countdownIncrement
+
+  setData()
+}
+
+const decrementCountdown = () => {
+  const countdownDecrement = _data.countdownTimeInMinutes - 5
+  if (countdownDecrement < 0) return
+
+  _data.countdownTimeInMinutes = countdownDecrement
+
+  setData()
+}
+
 function setData() {
   countdownMinuteSpan.innerText = String(_data.countdownTimeInMinutes).padStart(2, '0')
 }
@@ -52,6 +72,8 @@ function setData() {
 function registerDOMEvents() {
   countdownStartButton.addEventListener('click', () => startCountdown())
   countdownResetButton.addEventListener('click', () => resetCountdown())
+  countdownIncrementButton.addEventListener('click', () => incrementCountdown())
+  countdownDecrementButton.addEventListener('click', () => decrementCountdown())
 }
 
 function render() {
